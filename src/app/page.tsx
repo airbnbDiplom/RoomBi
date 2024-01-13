3b72d63283a8356b63d964c80185904073feae77
AlexABA91 <alex1991020480@gmail.com>
2024-01-13 17:53:03 +0200

    # - globals.css Добавил css переменные для цвета -layout.tsx добавил переменные шрифтов

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link'
+import style from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 style={{ color: "red", fontSize: "50px" }}>AIRBNB</h1>
-      <Link href="/posts">Posts</Link>
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<h1>AIRBNB</h1>
+			<Link href='/posts' className={style.font2}>
+				Posts
+			</Link>
+		</main>
+	)
 }
