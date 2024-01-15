e683e26ba98445f78849e2173356b49a86346758
AlexABA91 <alex1991020480@gmail.com>
2024-01-15 20:38:44 +0200

    изменение в global.css удалил лишнее

--- src/app/startComponents/header/Header.tsx
+++ src/app/startComponents/header/Header.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react'
-
+import { Row } from 'react-bootstrap'
 const Header: FC = () => {
-	return <div>header</div>
+	return (
+		<div>
+			<Row>RoomBi</Row>
+			<Row>Search </Row>
+		</div>
+	)
 }
 export { Header }
