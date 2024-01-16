2ba88b2ae498e6703f26ccd5cff2b5058884f5ae
AlexABA91 <alex1991020480@gmail.com>
2024-01-16 22:29:25 +0200

    Header.tsx - добавление адаптивной сетки -Header.modal.css добавлены классы для адоптивой сетки

--- src/app/startComponents/header/Header.tsx
+++ src/app/startComponents/header/Header.tsx
@@ -1,10 +1,53 @@
 import { FC } from 'react'
-import { Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
+import style from './Header.module.css'
 const Header: FC = () => {
 	return (
-		<div>
-			<Row>RoomBi</Row>
-			<Row>Search </Row>
+		<div className='ps-md-5 pe-md-5 ps-sm-3 pe-sm-3'>
+			<Row xl={12} className='pt-5'>
+				<Col className='text-center' sx={12} sm={2} md={2} lg={2} xl={2}>
+					Logo
+				</Col>
+				<Col sx={12} sm={10} md={10} lg={10} xl={10}>
+					<Row>
+						<Col
+							className={style.customText}
+							sx={6}
+							sm={6}
+							md={8}
+							lg={10}
+							xl={10}
+						>
+							Сдать жилье на RumBi
+						</Col>
+						<Col
+							className={(style.textCenter, style.customDisplayNone)}
+							sx={1}
+							sm={1}
+							md={1}
+							lg={1}
+							xl={1}
+						>
+							<p className='w-100 text-center p-0 m-0'>P</p>
+						</Col>
+						<Col
+							className={(style.customTextCenter, style.customDisplayNone)}
+							sx={5}
+							sm={5}
+							md={3}
+							lg={1}
+							xl={1}
+						>
+							AutButton
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+			<Row className='pt-5 pb-5 '>
+				<Col xs={12} className='text-center'>
+					Search
+				</Col>
+			</Row>
 		</div>
 	)
 }
--- src/app/startComponents/main/Main.tsx
+++ src/app/startComponents/main/Main.tsx
@@ -1,59 +1,59 @@
-import { CardBi } from "../../components/card/CardBi";
-import { Row, Col } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import style from "./main.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Col, Row } from 'react-bootstrap'
+import { CardBi } from '../../components/card/CardBi'
+import style from './main.module.css'
 
-import { cardData } from "../../tempData/data";
+import { cardData } from '../../tempData/data'
 
 const Main: React.FC = () => {
-  return (
-    <div className={style.container}>
-      <Row>
-        {cardData.map((item, index) => {
-          return (
-            <Col
-              key={item.id}
-              xs={{ span: 12 }}
-              sm={{ span: 6 }}
-              md={{ span: 4 }}
-              lg={{ span: 3 }}
-              xl={{ span: 2 }}
-            >
-              <CardBi {...item} />
-            </Col>
-          );
-        })}
-        {cardData.map((item, index) => {
-          return (
-            <Col
-              key={item.id}
-              xs={{ span: 12 }}
-              sm={{ span: 6 }}
-              md={{ span: 4 }}
-              lg={{ span: 3 }}
-              xl={{ span: 2 }}
-            >
-              <CardBi {...item} />
-            </Col>
-          );
-        })}
-        {cardData.map((item, index) => {
-          return (
-            <Col
-              key={item.id}
-              xs={{ span: 12 }}
-              sm={{ span: 6 }}
-              md={{ span: 4 }}
-              lg={{ span: 3 }}
-              xl={{ span: 2 }}
-            >
-              <CardBi {...item} />
-            </Col>
-          );
-        })}
-      </Row>
-    </div>
-  );
-};
+	return (
+		<div className={style.container}>
+			<Row>
+				{cardData.map((item, index) => {
+					return (
+						<Col
+							key={item.id}
+							xs={{ span: 12 }}
+							sm={{ span: 6 }}
+							md={{ span: 4 }}
+							lg={{ span: 3 }}
+							xl={{ span: 2 }}
+						>
+							<CardBi {...item} />
+						</Col>
+					)
+				})}
+				{cardData.map((item, index) => {
+					return (
+						<Col
+							key={item.id}
+							xs={{ span: 12 }}
+							sm={{ span: 6 }}
+							md={{ span: 4 }}
+							lg={{ span: 3 }}
+							xl={{ span: 2 }}
+						>
+							<CardBi {...item} />
+						</Col>
+					)
+				})}
+				{cardData.map((item, index) => {
+					return (
+						<Col
+							key={item.id}
+							xs={{ span: 12 }}
+							sm={{ span: 6 }}
+							md={{ span: 4 }}
+							lg={{ span: 3 }}
+							xl={{ span: 2 }}
+						>
+							<CardBi {...item} />
+						</Col>
+					)
+				})}
+			</Row>
+		</div>
+	)
+}
 
-export { Main };
+export { Main }
