067921f7f50ee1da5540ac94f0e7829159e443a8
AlexABA91 <alex1991020480@gmail.com>
2024-01-30 22:19:40 +0200

    # merge to max

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import { getAllHouses, getHousesBySearch } from "./services/housesServices";
-import { Header } from "./startComponents/header/Header";
-import { Main } from "./startComponents/main/Main";
-import { Naw } from "./startComponents/naw/Naw";
+import { getAllHouses } from './services/housesServices'
+import { Header } from './startComponents/header/Header'
+import { Main } from './startComponents/main/Main'
+import { Naw } from './startComponents/naw/Naw'
 
 // export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const cardData = await getAllHouses();
-  return (
-    <>
-      <div
-        className="headerBackgroundWhite  sticky-top ps-lg-5 pe-lg-5 ps-sm-3 pe-sm-3 ps-sx-3 pe-sx-3"
-        id="header"
-      >
-        {/* <Header /> */}
-        <Naw />
-      </div>
-      <main>
-        <Main cardData={cardData} />
-      </main>
-    </>
-  );
+	const cardData = await getAllHouses()
+	return (
+		<>
+			<div
+				className='headerBackgroundWhite  sticky-top ps-lg-5 pe-lg-5 ps-sm-3 pe-sm-3 ps-sx-3 pe-sx-3'
+				id='header'
+			>
+				<Header />
+				<Naw />
+			</div>
+			<main>
+				<Main cardData={cardData} />
+			</main>
+		</>
+	)
 }
--- src/app/startComponents/main/Main.tsx
+++ src/app/startComponents/main/Main.tsx
@@ -1,46 +1,45 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Row } from "react-bootstrap";
-import { CardBi } from "../../components/card/CardBi";
-import { ButtonShowMore } from "../../ui/buttonShowMore/ButtonShowMore";
-import style from "./main.module.css";
-import { CardBiProps } from "@/app/type/type";
-import Loading from "../../loading";
-import { FileTest } from "../../tempData/FileTest";
+import { CardBiProps } from '@/app/type/type'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Col, Row } from 'react-bootstrap'
+import { CardBi } from '../../components/card/CardBi'
+import Loading from '../../loading'
+import { ButtonShowMore } from '../../ui/buttonShowMore/ButtonShowMore'
+import style from './main.module.css'
 const Main: React.FC<{ cardData: CardBiProps[] }> = ({
-  cardData,
+	cardData,
 }: {
-  cardData: CardBiProps[];
+	cardData: CardBiProps[]
 }) => {
-  if (cardData) {
-    return (
-      <div
-        className={`${style.container} ms-lg-5 me-lg-5 ms-sm-3 me-sm-3 ms-sx-3 me-sx-3`}
-      >
-        <FileTest />
-        <Row>
-          {cardData.map((item) => {
-            return (
-              <Col
-                key={item.id}
-                xs={{ span: 12 }}
-                sm={{ span: 6 }}
-                md={{ span: 4 }}
-                lg={{ span: 3 }}
-                xl={{ span: 2 }}
-              >
-                <CardBi {...item} />
-              </Col>
-            );
-          })}
-        </Row>
-        <ButtonShowMore />
-      </div>
-    );
-  }
-  return (
-    <div>
-      <Loading />
-    </div>
-  );
-};
-export { Main };
+	if (cardData) {
+		return (
+			<div
+				className={`${style.container} ms-lg-5 me-lg-5 ms-sm-3 me-sm-3 ms-sx-3 me-sx-3`}
+			>
+				{/* <FileTest /> */}
+				<Row>
+					{cardData.map(item => {
+						return (
+							<Col
+								key={item.id}
+								xs={{ span: 12 }}
+								sm={{ span: 6 }}
+								md={{ span: 4 }}
+								lg={{ span: 3 }}
+								xl={{ span: 2 }}
+							>
+								<CardBi {...item} />
+							</Col>
+						)
+					})}
+				</Row>
+				<ButtonShowMore />
+			</div>
+		)
+	}
+	return (
+		<div>
+			<Loading />
+		</div>
+	)
+}
+export { Main }
