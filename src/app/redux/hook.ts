1742249d2a94c021e2a2454b1c432db4379ff00e
AlexABA91 <alex1991020480@gmail.com>
2024-02-16 23:45:07 +0200

    #error ocurred

--- src/app/redux/hook.ts
+++ src/app/redux/hook.ts
@@ -5,10 +5,13 @@ import { useEffect, useState } from 'react'
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export const useWindowSize = () => {
-	const [size, setSize] = useState([window.innerWidth, window.innerHeight])
+	const [size, setSize] = useState([0, 0])
 	useEffect(() => {
-		const handleResize = () => setSize([window.innerWidth, window.innerHeight])
+		const handleResize = () =>
+			setSize([window?.innerWidth, window?.innerHeight])
+
 		window.addEventListener('resize', handleResize)
+
 		return () => window.removeEventListener('resize', handleResize)
 	}, [])
 	return size
