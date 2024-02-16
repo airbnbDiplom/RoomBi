21502ccc3edd99aff63ea58f8b22934bfd66519b
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2024-02-17 00:15:33 +0300

    rtedl4

--- src/app/services/housesServices.ts
+++ src/app/services/housesServices.ts
@@ -14,18 +14,53 @@ const getHouses = async () => {
   if (!response.ok) throw new Error("Unable to fetch houses.");
   return response.json();
 };
+// const getFirstPageServer = async () => {
+//   const url = process.env.NEXT_GET_FIRST_PAGE;
+//   if (!url) {
+//     throw new Error("URL is undefined.");
+//   }
+//   const response = await fetch(url, {
+//     next: {
+//       revalidate: 500,
+//     },
+//   });
+//   if (!response.ok) throw new Error("Unable to fetch houses.");
+//   return response.json();
+// };
+
+const fetch = require("node-fetch");
+
 const getFirstPageServer = async () => {
   const url = process.env.NEXT_GET_FIRST_PAGE;
   if (!url) {
     throw new Error("URL is undefined.");
   }
-  const response = await fetch(url, {
-    next: {
-      revalidate: 500,
-    },
-  });
-  if (!response.ok) throw new Error("Unable to fetch houses.");
-  return response.json();
+
+  const timeout = 20000; // Установите время ожидания в миллисекундах (в данном случае 10 секунд)
+
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      signal: controller.signal,
+      next: {
+        revalidate: 5,
+      },
+    });
+
+    clearTimeout(timeoutId); // Очистите таймер тайм-аута после получения ответа
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch houses.");
+    }
+
+    return response.json();
+  } catch (error) {
+    clearTimeout(timeoutId); // Очистите таймер тайм-аута при возникновении ошибки
+    throw error;
+  }
 };
 export const getFirstPage = async () => {
   const response = await getFirstPageServer();
