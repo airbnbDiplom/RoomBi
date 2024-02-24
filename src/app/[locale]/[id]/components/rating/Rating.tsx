444883fa57a867bbc807429f53f88ec824c31744
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2024-02-24 10:09:51 +0300

    test pageInfo2

--- src/app/[locale]/[id]/components/rating/Rating.tsx
+++ src/app/[locale]/[id]/components/rating/Rating.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { RentalApartmentDTO } from "@/app/type/type";
 import style from "./rating.module.css";
-
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
 const Rating: React.FC<{ data: RentalApartmentDTO }> = ({
