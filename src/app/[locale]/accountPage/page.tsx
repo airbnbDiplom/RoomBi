b2c43ba3d82c90ce8f7d695cfbb5861c41f06027
tom.k.u90 <tom.k.u90@gmail.com>
2024-03-02 22:53:42 +0200

    register

---
+++ src/app/[locale]/accountPage/page.tsx
@@ -0,0 +1,42 @@
+import TranslationsProvider from '@/app/configs/TranslationsProvider'
+import initTranslations from '@/app/i18n'
+import { Footer } from '@/app/startComponents/footer/Footer'
+import { Header } from '@/app/startComponents/header/Header'
+import { HomeParams } from '@/app/type/type'
+import React from 'react'
+import styles from './account.module.css'
+
+const i18nNamespaces = ['translation']
+
+export const AccountPage = async ({
+  params: { locale },
+}: {
+  params: HomeParams
+}) => {
+  const { resources } = await initTranslations(locale, ['translation'])
+
+  return (
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}
+    >
+      <div className={styles.pageContainer}>
+        <div className='header-main'>
+          <Header />
+        </div>
+        <div className={styles.content}>
+          <h1>Account</h1>
+          <div>
+            <h2>Personal Information</h2>
+            <p>Name: </p>
+            <p>Email: </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </TranslationsProvider>
+  )
+}
+
+export default AccountPage
