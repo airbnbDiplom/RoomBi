655d3dce8e95fcbe9cc491dc4f081c02bd473402
AlexABA91 <alex1991020480@gmail.com>
2024-04-03 10:35:49 +0300

    commit

--- src/app/[locale]/searchResult/components/filterMain.tsx
+++ src/app/[locale]/searchResult/components/filterMain.tsx
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 import React from 'react'
 import style from '@/app/[locale]/searchResult/searchResult.module.css'
 import SearchResult from './searchResult'
--- src/app/[locale]/searchResult/page.tsx
+++ src/app/[locale]/searchResult/page.tsx
@@ -1,19 +1,33 @@
+'use client'
 import TranslationsProvider from '@/app/configs/TranslationsProvider'
 import initTranslations from '@/app/i18n'
 import { Footer } from '@/app/startComponents/footer/Footer'
 import { Header } from '@/app/startComponents/header/Header'
 import { HomeParams } from '@/app/type/type'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FilterMain from './components/filterMain'
 
 const i18nNamespaces = ['translation']
 
-export default async function SearchDataView({
+export default function SearchDataView({
 	params: { locale },
 }: {
 	params: HomeParams
 }) {
-	const { resources } = await initTranslations(locale, ['translation'])
+	const [resources, setResources] = useState(null)
+
+	useEffect(() => {
+		const fetchTranslations = async () => {
+			const { resources } = await initTranslations(locale, ['translation'])
+			setResources(resources)
+		}
+		fetchTranslations()
+	}, [locale])
+
+	if (!resources) {
+		// Optional: You can return a loading indicator here if needed
+		return null
+	}
 
 	return (
 		<TranslationsProvider
