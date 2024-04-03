b0d415283743d0a077bb6979a412cc81316ff5ca
AlexABA91 <alex1991020480@gmail.com>
2024-04-03 11:40:05 +0300

    build success

--- src/app/[locale]/searchResult/components/filterMain.tsx
+++ src/app/[locale]/searchResult/components/filterMain.tsx
@@ -1,10 +1,24 @@
-'use client'
-import React from 'react'
+'use server'
+import React, { useMemo } from 'react'
 import style from '@/app/[locale]/searchResult/components/searchResult.module.css'
 import SearchResultList from './searchResultList'
-import MapForSearch from './mapForSearch'
+import dynamic from 'next/dynamic'
 
 const FilterMain = () => {
+	const MapForSearch = useMemo(
+		() =>
+			dynamic(
+				() =>
+					import('@/app/[locale]/searchResult/components/mapForSearch').then(
+						mod => mod.default
+					),
+				{
+					// loading: () => <Loading />,
+					ssr: false,
+				}
+			),
+		[]
+	)
 	return (
 		<main className={style.main}>
 			<SearchResultList />
